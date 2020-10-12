c2a28c09d01d1cf925cdc49ea9596050cf057cad
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-12 17:26:54 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('toni', function () {
     return view('welcome');
+});
+
+Route::get('/bem-vindo', function () {
+    echo('olá mundo');
+});
+
+Route::get('/', function () {
+   echo('<h1>Olá mundo</h1>'); 
+});
+
+Route::get('/bem-vindo/aedah', function (){
+    echo('<h1>esta é só para o Agrupamento :)</h1>');
+});
+
+Route::get('/nome/{nome}', function ($nome){
+    echo('<h1>Olá '. $nome .'</h1>');
+});
+
+Route::get('/nome/{nome}/{apelido}', function ($nome, $apelido){
+    echo('<h1>Olá '. $nome .' '. $apelido .'</h1>');
+});
+
+Route::get('/nomes/{nome}/{vezes}',function($nome, $vezes) {
+    $vezes=is_numeric ($vezes) ? $vezes : 5;
+    for($i=1; $i <= $vezes; $i++){
+        echo($nome);
+        echo('<br>');
+    }
 });
